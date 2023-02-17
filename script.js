25481e1ff4fcaa7d816a096073aafb7183fe38b5
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-02-17 13:39:41 -0800

    Create the getComputerChoice() function
    Used Math.floor and Math.random to make a random selection between 1 and
    3, and returned Rock, Paper, or Scissors as a string value.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1) {
+    computerChoice = "Rock";
+  } else if (computerChoice === 2) {
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  }
   return computerChoice;
 }
