cadf8d36b838f9fbdc492240cf46ceff144f6b16
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-02-21 13:31:47 -0800

    Add first pass at playRound logic

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice === 0) {
-    computerChoice = "Rock";
+    computerChoice = "rock";
   } else if (computerChoice === 1) {
-    computerChoice = "Paper";
+    computerChoice = "paper";
   } else {
-    computerChoice = "Scissors";
+    computerChoice = "scissors";
   }
   return computerChoice;
 }
+
+function playRound(playerChoice, computerChoice) {
+  playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+  if (playerChoice === computerChoice) {
+    return "It's a draw!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "You win this round!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "scissors") ||
+    (playerChoice === "scissors" && computerChoice === "rock")
+  ) {
+    return "You lose this round!";
+  }
+}
+
+getComputerChoice();
+playRound();
