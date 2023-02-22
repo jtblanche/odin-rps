8ee97481dc6d7eaabb22c22956001e9a2cddfc72
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-02-22 14:58:57 -0800

    Successfully complete 5 rounds and report winner and score Finally! I struggled a lot and it probably took ~3-4 hours of total time but I have a working version of JS console rock paper scissors. The syntax and choices I made probably are far from ideal, but for now the logic works and I had a few "aha!" moments as I debugged issues. There are a lot of edge case issues/bugs I want to solve later like if a player enters something other than rock paper scissors, hits cancel, etc. But this code runs error free as long as the player behaves.

--- script.js
+++ script.js
@@ -19,30 +19,38 @@ function playRound(playerChoice, computerChoice) {
   playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
   computerChoice = getComputerChoice();
   if (playerChoice === computerChoice) {
-    return `It's a tie - you both chose ${playerChoice}!`;
+    return `It's a tie - you both chose ${playerChoice}! Player: ${playerScore}  Computer: ${computerScore}`;
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
     playerScore++;
-    return `You win this round - ${playerChoice} beats ${computerChoice}!`;
+    return `You win this round - ${playerChoice} beats ${computerChoice}! Player: ${playerScore}  Computer: ${computerScore}`;
   } else if (
     (playerChoice === "rock" && computerChoice === "paper") ||
     (playerChoice === "paper" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "rock")
   ) {
     computerScore++;
-    return `You lose this round - ${playerChoice} is beaten by ${computerChoice}!`;
+    return `You lose this round - ${playerChoice} is beaten by ${computerChoice}! Player: ${playerScore}  Computer: ${computerScore}`;
   }
 }
 
-// console.log(playRound(playerChoice, computerChoice));
-
 function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(playerChoice, computerChoice));
+  function winner() {
+    if (playerScore > computerScore) {
+      return `You win! Final score - Player: ${playerScore}  Computer: ${computerScore}`;
+    } else if (playerScore < computerScore) {
+      return `You lose! Final score - Player: ${playerScore}  Computer: ${computerScore}`;
+    } else {
+      return `Tie game! Final score - Player: ${playerScore}  Computer: ${computerScore}`;
+    }
   }
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
+  }
+  console.log(winner());
 }
 
 game();
