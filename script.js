2d9ab4ef1c3c1d65e6266e5e49f687c54bab980f
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-03-15 11:06:16 -0700

    Results of each round are updated in the DOM paragraph

--- script.js
+++ script.js
@@ -28,44 +28,36 @@ function playRound() {
   }
   let computerChoice = getComputerChoice();
   if (playerChoice === computerChoice) {
-    console.log(`It's a tie - you both chose ${playerChoice}!`);
+    document.getElementById(
+      "results-text"
+    ).innerHTML = `It's a tie - you both chose ${playerChoice}!`;
+    // console.log(`It's a tie - you both chose ${playerChoice}!`);
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
     playerScore++;
-    console.log(
-      `You win this round - ${playerChoice} beats ${computerChoice}!`
-    );
+    document.getElementById(
+      "results-text"
+    ).innerHTML = `You win this round - ${playerChoice} beats ${computerChoice}!`;
+    // console.log(
+    // `You win this round - ${playerChoice} beats ${computerChoice}!`
   } else if (
     (playerChoice === "rock" && computerChoice === "paper") ||
     (playerChoice === "paper" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "rock")
   ) {
     computerScore++;
-    console.log(
-      `You lose this round - ${playerChoice} is beaten by ${computerChoice}!`
-    );
+    document.getElementById(
+      "results-text"
+    ).innerHTML = `You lose this round - ${playerChoice} is beaten by ${computerChoice}!`;
+    // console.log(
+    //   `You lose this round - ${playerChoice} is beaten by ${computerChoice}!`
   }
   document.getElementById("c-score").innerHTML = computerScore;
   document.getElementById("p-score").innerHTML = playerScore;
 }
 
-// function game() {
-//   function winner() {
-//     if (playerScore > computerScore) {
-//       return `You win! Final score - Player: ${playerScore}  Computer: ${computerScore}`;
-//     } else if (playerScore < computerScore) {
-//       return `You lose! Final score - Player: ${playerScore}  Computer: ${computerScore}`;
-//     } else {
-//       return `Tie game! Final score - Player: ${playerScore}  Computer: ${computerScore}`;
-//     }
-//   }
-//   for (let i = 0; i < 5; i++) {
-//     console.log(playRound());
-//   }
-//   console.log(winner());
-// }
-
-// game();
+// add logic where game ends at 5 with a win/lose message
+// add reset button logic
